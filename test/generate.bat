6be6e4cc99fbb929ef5c4e07214cfd625991dc1d
kouichiUme <kouichiume@gmail.com>
2020-10-14 23:54:33 +0900

    generate command

---
+++ test/generate.bat
@@ -0,0 +1,4 @@
+main.py .\PspSupportQualityMeasure\test\Hogehoge.java
+rem 親ディレクトリに　grammars-v4をダウンロードしておく
+rem git clone https://github.com/antlr/grammars-v4.git
+java -jar antlr-4.8-complete.jar -Dlanguage=Python3 grammars-v4\java\java9\Java9.g4
